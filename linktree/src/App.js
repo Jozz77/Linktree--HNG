fb6d67eb0464a0e1fd446f15555c5eace8c14c72
Jozz77 <mmadujonathan@gmail.com>
2022-10-24 16:25:08 +0100

    add the footer to the page

--- linktree/src/App.js
+++ linktree/src/App.js
@@ -4,33 +4,40 @@ import github from './img/github.png'
 import slack from './img/slack.png'
 import zuri from './img/Zuri-Internship.svg'
 import I4G from './img/I4G.svg'
+import share from './img/share (2).png'
 
 function App() {
   return (
     <section className="linktree">
       <div className="profile">
       <img src={jo} alt="Logo" id="profile__img" /> 
-      <img src="../public/img/Jonathan.jfif" alt="Logo" id="profile__img" /> 
+      {/* <img src="../public/img/Jonathan.jfif" alt="Logo" id="profile__img" />  */}
       <h3>Jonathan Mmadu</h3>
       </div>
 
+      <p id="slack">Jo_Na_Than</p>
+      <div className="share">
+        <img src={share} alt="Logo" id="profile__img" /> 
+      </div>
+
       <div className="links">
-      <a href="">Twitter Link</a>
-      <a href="">Zuri Team</a>
-      <a href="">Zuri Books</a>
-      <a href="">Background Check for Coders</a>
-      <a href="">Design Books</a>
+      <a href="https://twitter.com/JonathanOnyedi3" target="_blank" id="twitter">Twitter Link</a>
+      <a href="" id="btn_zuri" target="_blank">Zuri Team</a>
+      <a href="" id="books" target="_blank">Zuri Books</a>
+      <a href="" id="pitch" target="_blank">Background Check for Coders</a>
+      <a href="" id="book_design" target="_blank">Design Books</a>
       </div>
 
       <div className="git-slack">
-      <img src={github} alt="Logo" id="profile__img" />
-      <img src={slack} alt="Logo" id="profile__img" />  
+      <a href=""> <img src={github} alt="Logo" id="profile__img" /></a>
+      <a href=""><img src={slack} alt="Logo" id="profile__img" /> </a>
       </div>
 
+      <hr></hr>
       <footer>
-      <img src={zuri} alt="Logo" id="profile__img" />
+      <a href=""><img src={zuri} alt="Logo" id="profile__img" /></a>
       <p>HNG Internship 9 Frontend Task</p>
-      <img src={I4G} alt="Logo" id="profile__img" />
+      <a href=""><img src={I4G} alt="Logo" id="profile__img" /></a>
       </footer>
     </section>
   );
