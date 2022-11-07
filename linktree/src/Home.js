b6cd7af2907720c694059763c106f0b970caf643
Jozz77 <mmadujonathan@gmail.com>
2022-11-07 15:46:31 +0100

    Feature: put in the correct id name

--- linktree/src/Home.js
+++ linktree/src/Home.js
@@ -29,7 +29,7 @@ export default function Home() {
 
       <div className="links">
       <a href="https://twitter.com/JonathanOnyedi3" target="_blank" id="twitter">Twitter Link</a>
-      <a href="https://training.zuri.team/" id="btn_zuri" target="_blank">Zuri Team</a>
+      <a href="https://training.zuri.team/" id="btn__zuri" target="_blank">Zuri Team</a>
       <a href="http://books.zuri.team/" id="books" target="_blank">Zuri Books</a>
       <a href="https://books.zuri.team/python-for-beginners?ref_id=<Jo_Na_Than>" id="book__python" target="_blank">Python Books</a>
       <a href="https://background.zuri.team" id="pitch" target="_blank">Background Check for Coders</a>
